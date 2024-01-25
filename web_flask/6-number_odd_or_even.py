477148b73f15b8cb1b228a31fe121fa6caa8cbf8
kelechukwufavour <kelechukwufavour.faye@gmail.com>
2024-01-25 22:11:10 +0000

    added the correct port

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -53,11 +53,11 @@ def number_template(n):
     return render_template('5-number.html', n=n)
 
 
+@app.route('/airbnb-dynamics/number_odd_or_even/<int:n>', strict_slashes=False)
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     """display a HTML page if n is int"""
     return render_template('6-number_odd_or_even.html', n=n)
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5001)
